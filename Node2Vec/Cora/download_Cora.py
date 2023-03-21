85ce98cb4b32b0d01d85ef1bf84ae1a7a934f755
Haotian Shen <haotian.shen18@gmail.com>
2023-03-21 21:43:52 +0000

    Change Account

--- Node2Vec/Cora/download_Cora.py
+++
@@ -1,6 +0,0 @@
-from torch_geometric.datasets import Planetoid
-import os.path as osp
-
-dataset = 'Cora'
-path = osp.join(osp.dirname(osp.realpath(__file__)), dataset)
-dataset = Planetoid(path, dataset)
--- Node2Vec/Cora/run.sh
+++
@@ -1,7 +0,0 @@
-for dim in 8 16 32 64 128 256
-do
-    for seed in {0..9}
-    do 
-        python generate_embeddings.py --embedding_dim $dim --seed $seed
-    done
-done
